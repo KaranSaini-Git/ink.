70a37181977255d0210891fe5144979f1c282699
KaranSaini-Git <karansaini.git@gmail.com>
2026-03-28 16:03:01 +0530

    Add Commenting in posts

--- blog/views.py
+++ blog/views.py
@@ -70,18 +70,23 @@ def post_list(request):
 
     return render(request,'blog/post/list.html',{'posts':posts})
 
-def post_detail(request,year,month,day,post):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
-    Post,
-    status=Post.Status.PUBLISHED,
-    slug=post,
-    publish__year=year,
-    publish__month=month,
-    publish__day=day
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
-    return render(request,'blog/post/detail.html',{'post':post})
+    comments = post.comments.filter(active=True)  # get approved comments
+    form = CommentForm()                           # empty form for the page
 
-# Handling model forms in vies
+    return render(request, 'blog/post/detail.html', {
+        'post': post,
+        'comments': comments,
+        'form': form,
+    })
 @require_POST
 def post_comment(request, post_id):
     post = get_object_or_404(
