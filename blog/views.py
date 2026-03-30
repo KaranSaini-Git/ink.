99dae5ae77dc7828c1e80d4e4f412a363d96552e
KaranSaini-Git <karansaini.git@gmail.com>
2026-03-30 13:39:08 +0530

    Add feature of retriving posts by similarity and making the tags in side bar interactive

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render
-from . models import Post
-from django.shortcuts import redirect
-from django.shortcuts import get_object_or_404,render
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from taggit.models import Tag
-# from .forms import EmailPostForm
-from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.db.models import Count
+from taggit.models import Tag
+from .models import Post
+from .forms import EmailPostForm, CommentForm
 
+
+######## post_share
 def post_share(request, post_id):
-    # Retrieve post by id
     post = get_object_or_404(
         Post,
         id=post_id,
@@ -19,14 +18,10 @@ def post_share(request, post_id):
     )
     sent = False
     if request.method == 'POST':
-        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            # Form fields passed validation
             cd = form.cleaned_data
-            post_url = request.build_absolute_uri(
-            post.get_absolute_url()
-            )
+            post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = (
                 f"{cd['name']} ({cd['email']}) "
                 f"recommends you read {post.title}"
@@ -42,11 +37,16 @@ def post_share(request, post_id):
                 recipient_list=[cd['to']]
             )
             sent = True
-        # ... send email
     else:
         form = EmailPostForm()
-    return render(request,'blog/post/share.html',{'post': post,'form': form,'sent': sent})
+    return render(request, 'blog/post/share.html', {
+        'post': post,
+        'form': form,
+        'sent': sent,
+    })
 
+
+######## PostListView
 class PostListView(ListView):
     """
     Alternative post list view
@@ -56,25 +56,31 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
-def post_list(request,tag_slug=None):
-    post_list=Post.published.all()
+
+######## post_list
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
     tag = None
     if tag_slug:
-        tag = get_object_or_404(Tag,Slug = tag_slug)
+        tag = get_object_or_404(Tag, slug=tag_slug)
         post_list = post_list.filter(tags__in=[tag])
-    paginator=Paginator(post_list,3)
-    page_number=request.GET.get('page',1)
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
     try:
-        posts=paginator.page(page_number)
+        posts = paginator.page(page_number)
     except PageNotAnInteger:
-# If page_number is not an integer get the first page
         posts = paginator.page(1)
     except EmptyPage:
-# If page_number is out of range get last page of resul
         posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {
+    'posts': posts,
+    'tag': tag,
+    'all_tags': Tag.objects.all(),
+    'active_tag': tag,  # 👈 this is what drives the highlight
+})
 
-    return render(request,'blog/post/list.html',{'posts':posts,'tag':tag})
 
+######## post_detail
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
@@ -84,14 +90,24 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
-    comments = post.comments.filter(active=True)  # get approved comments
-    form = CommentForm()                           # empty form for the page
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+    # Similar posts by shared tags
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
 
     return render(request, 'blog/post/detail.html', {
         'post': post,
         'comments': comments,
         'form': form,
+        'similar_posts': similar_posts,
+        'all_tags': Tag.objects.all(),
     })
+
+
+######## post_comment
 @require_POST
 def post_comment(request, post_id):
     post = get_object_or_404(
@@ -101,7 +117,6 @@ def post_comment(request, post_id):
     )
     comment = None
     form = CommentForm(data=request.POST)
-
     if form.is_valid():
         comment = form.save(commit=False)
         comment.post = post
